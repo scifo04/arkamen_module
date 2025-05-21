cafa7ef78e2d13d4fd2c2d4a27ed9eb1e2e27856
andhitanh <onecodhita@gmail.com>
2025-05-21 17:53:53 +0700

    feat : laporan utilisasi eksternal

--- models/utilization_report.py
+++ models/utilization_report.py
@@ -1,9 +1,9 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class UtilizationReport(models.TransientModel):
-    _name = 'arkamen.utilization.report'
-    _description = 'Laporan Utilisasi SDM'
+class UtilizationReportInternal(models.TransientModel):
+    _name = 'arkamen.utilization.report.internal'
+    _description = 'Laporan Utilisasi SDM Internal'
 
     start_date = fields.Date(string="Tanggal Mulai", required=True, default=fields.Date.today)
     end_date = fields.Date(string="Tanggal Selesai", required=True, default=fields.Date.today)
@@ -17,7 +17,6 @@ class UtilizationReport(models.TransientModel):
     
     def action_generate_report(self):
         self.env['arkamen.utilization.report.internal.line'].search([]).unlink()
-        self.env['arkamen.utilization.report.external.line'].search([]).unlink()
         
         allocations = self.env['arkamen.allocate'].search([
             ('start_date', '<=', self.end_date),
@@ -36,26 +35,7 @@ class UtilizationReport(models.TransientModel):
                 self.env['arkamen.utilization.report.internal.line'].create({
                     'report_id': self.id,
                     'employee_name': user.full_name,
-                    'skills': user.display_skills,
                     'skill_level': skill_level_label,
-                    'project': project.project_name,
-                    'vendor': vendor_name,
-                    'start_date': allocation.start_date,
-                    'end_date': allocation.end_date,
-                    'allocation_percentage': allocation.allocation_percentage,
-                })
-        
-        for allocation in allocations:
-            project = allocation.project
-            vendor = self.env['arkamen.vendor'].search([('project', '=', project.id)], limit=1)
-            vendor_name = vendor.vendor_name if vendor else ''
-            
-            for user in allocation.external_user:
-                self.env['arkamen.utilization.report.external.line'].create({
-                    'report_id': self.id,
-                    'employee_name': user.full_name,
-                    'skills': user.display_skills,
-                    'tariff': user.tariff,
                     'project': project.project_name,
                     'vendor': vendor_name,
                     'start_date': allocation.start_date,
@@ -69,18 +49,66 @@ class UtilizationReport(models.TransientModel):
             'res_model': 'arkamen.utilization.report.internal.line',
             'view_mode': 'list',
             'domain': [('report_id', '=', self.id)],
-            'target': 'current',
         }
         
         return action_internal
+    
+class UtilizationReportExternal(models.TransientModel):
+    _name = 'arkamen.utilization.report.external'
+    _description = 'Laporan Utilisasi SDM Eksternal'
+
+    start_date = fields.Date(string="Tanggal Mulai", required=True, default=fields.Date.today)
+    end_date = fields.Date(string="Tanggal Selesai", required=True, default=fields.Date.today)
+    
+    @api.constrains('start_date', 'end_date')
+    def _check_dates(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                if record.start_date > record.end_date:
+                    raise ValidationError("Tanggal mulai harus lebih awal daripada tanggal selesai.")
+    
+    def action_generate_report(self):
+        self.env['arkamen.utilization.report.external.line'].search([]).unlink()
+        
+        allocations = self.env['arkamen.allocate'].search([
+            ('start_date', '<=', self.end_date),
+            ('end_date', '>=', self.start_date)
+        ])
+        
+        for allocation in allocations:
+            project = allocation.project
+            vendor = self.env['arkamen.vendor'].search([('project', '=', project.id)], limit=1)
+            vendor_name = vendor.vendor_name if vendor else ''
+            
+            for user in allocation.external_user:
+                self.env['arkamen.utilization.report.external.line'].create({
+                    'report_id': self.id,
+                    'employee_name': user.full_name,
+                    'tariff': user.tariff,
+                    'work_review': user.work_review_ids,
+                    'project': project.project_name,
+                    'vendor': vendor_name,
+                    'start_date': allocation.start_date,
+                    'end_date': allocation.end_date,
+                    'allocation_percentage': allocation.allocation_percentage,
+                })
+        
+        action_external = {
+            'name': 'Data Utilisasi Karyawan Ektsernal',
+            'type': 'ir.actions.act_window',
+            'res_model': 'arkamen.utilization.report.external.line',
+            'view_mode': 'list',
+            'domain': [('report_id', '=', self.id)],
+        }
+        
+        return action_external
     
 class UtilizationReportInternalLine(models.TransientModel):
     _name = 'arkamen.utilization.report.internal.line'
     _description = 'Baris Laporan Utilisasi Karyawan Internal'
     
-    report_id = fields.Many2one('arkamen.utilization.report', string="Report")
+    report_id = fields.Many2one('arkamen.utilization.report.internal', string="Report")
     employee_name = fields.Char(string="Nama Karyawan")
-    skills = fields.Char(string="List of Skills")
     skill_level = fields.Char(string="Skill Level")
     project = fields.Char(string="Proyek yang Dikerjakan")
     vendor = fields.Char(string="Vendor Proyek")
@@ -92,11 +120,11 @@ class UtilizationReportExternalLine(models.TransientModel):
     _name = 'arkamen.utilization.report.external.line'
     _description = 'Baris Laporan Utilisasi Karyawan Eksternal'
     
-    report_id = fields.Many2one('arkamen.utilization.report', string="Report")
+    report_id = fields.Many2one('arkamen.utilization.report.external', string="Report")
     employee_name = fields.Char(string="Nama Karyawan")
-    skills = fields.Char(string="List of Skills")
     tariff = fields.Integer(string="Tariff")
     project = fields.Char(string="Proyek yang Dikerjakan")
+    work_review = fields.Char(string="Work Review")
     vendor = fields.Char(string="Vendor Proyek")
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
